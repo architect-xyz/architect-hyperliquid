1866e063b87ed0868594c1cb79ef26582556eae9
Andrew Lee <665282+chairmanlee8@users.noreply.github.com>
2025-03-30 13:35:25 -0500

    scaffolding

---
+++ main.py
@@ -0,0 +1,66 @@
+import time
+import grpc
+from concurrent import futures
+from architect_py.grpc_client.Cpty.CptyRequest import CancelOrder, PlaceOrder
+from architect_py.grpc_client.Cpty.CptyResponse import Symbology
+from architect_py.grpc_client.definitions import CptyLoginRequest, CptyLogoutRequest
+from architect_py.grpc_client.grpc_server import (
+    add_CptyServicer_to_server,
+    CptyServicer,
+    add_OrderflowServicer_to_server,
+    OrderflowServicer,
+)
+
+
+class MockCptyServicer(CptyServicer, OrderflowServicer):
+    def Cpty(self, request_iterator, context):
+        context.set_code(grpc.StatusCode.OK)
+        context.send_initial_metadata({})
+        # send symbology
+        yield Symbology(
+            execution_info={
+                "FOO Crypto/USD": {
+                    "MOCK": {
+                        "execution_venue": "MOCK",
+                        "exchange_symbol": None,
+                        "tick_size": {"simple": "0.01"},
+                        "step_size": "0.1",
+                        "min_order_quantity": "0",
+                        "min_order_quantity_unit": {"unit": "base"},
+                        "is_delisted": False,
+                        "initial_margin": None,
+                        "maintenance_margin": None,
+                    }
+                }
+            }
+        )
+        for req in request_iterator:
+            if isinstance(req, CptyLoginRequest):
+                print("login message received", req)
+            elif isinstance(req, CptyLogoutRequest):
+                print("logout message received", req)
+            elif isinstance(req, PlaceOrder):
+                print("place_order message received", req)
+            elif isinstance(req, CancelOrder):
+                print("cancel_order message received", req)
+
+    def SubscribeOrderflow(self, request, context):
+        context.set_code(grpc.StatusCode.OK)
+        context.send_initial_metadata({})
+        time.sleep(100)
+
+
+def main():
+    thread_pool = futures.ThreadPoolExecutor(max_workers=10)
+    server = grpc.server(thread_pool)
+    servicer = MockCptyServicer()
+    add_CptyServicer_to_server(servicer, server)
+    add_OrderflowServicer_to_server(servicer, server)
+    server.add_insecure_port("[::]:50051")
+    server.start()
+    print("server started on port 50051")
+    server.wait_for_termination()
+
+
+if __name__ == "__main__":
+    main()
